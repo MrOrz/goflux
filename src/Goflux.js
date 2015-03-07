84f09d2d9d1d726381756a2bd2e3b3dbf61ce5d1
Tom Chen <developer@tomchentw.com>
2015-03-07 15:41:48 +0800

    feat(Goflux): create defineActions instance method

--- src/Goflux.js
+++ src/Goflux.js
@@ -1,5 +1,13 @@
+import ActionDescriptor from "./internals/ActionDescriptor";
+
 class Goflux {
 
+  static defineActions (actionName, actionFactory) {
+    return new ActionDescriptor(actionName, actionFactory);
+  }
+
+  constructor () {
+  }
 }
 
 export default Goflux;
---
+++ src/internals/ActionDescriptor.js
@@ -0,0 +1,9 @@
+class ActionDescriptor {
+
+  constructor (name, factory) {
+    this._name = name;
+    this._factory = factory;
+  }
+}
+
+export default ActionDescriptor;
