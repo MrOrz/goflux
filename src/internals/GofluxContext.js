e61876854305e5e51068a77eb0fd232eb1eaf938
Tom Chen <developer@tomchentw.com>
2015-03-07 16:24:23 +0800

    feat(GofluxContext): create restricted context in constructor

--- src/internals/GofluxContext.js
+++ src/internals/GofluxContext.js
@@ -1,3 +1,5 @@
+import invariant from "flux/lib/invariant";
+
 class GofluxContext {
 
   dispatch (eventName, payload) {
@@ -17,6 +19,37 @@ class GofluxContext {
   }
 
   constructor () {
+    this._publicContext = this._create_restricted_context("public", {
+      getActions: true,
+    });
+
+    this._actionsContext = this._create_restricted_context("actions", {
+      dispatch: true,
+      getActions: true,
+    });
+
+    this._storeContext = this._create_restricted_context("store", {
+      waitFor: true,
+      getStore: true,
+    });
+  }
+
+  _create_restricted_context (restrictedName, validMethodNameMappings) {
+    function restrictFn () {
+      invariant(false, "function called on restricted (%s) context.", restrictedName);
+    }
+
+    const propertiesObject = {};
+    for (var name in GofluxContext.prototype) {
+      if (validMethodNameMappings[name]) {
+        continue;
+      }
+      propertiesObject[name] = {
+        value: restrictFn,
+      };
+    }
+
+    return Object.create(this, propertiesObject);
   }
 
   _initialize_ (actionsDescriptorsMap, storeDescriptorsMap) {
