3c9e6e4c7ad313219c6917d42a297d44568421a2
Tom Chen <developer@tomchentw.com>
2015-04-12 10:58:41 +0800

    feat(goflux): change export structure
    BREAKING CHANGE: change export structure
    
    * Goflux class is stored under Goflux key
    * mixins are collected in a hash by mixins key

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import Goflux from "./Goflux";
 import GofluxMixin from "./mixins/GofluxMixin";
 import StoreWatchMixin from "./mixins/StoreWatchMixin";
 
-export default Goflux;
-export {
+const mixins = {
   GofluxMixin,
   StoreWatchMixin,
+};
+
+export {
+  Goflux,
+  mixins,
 };
